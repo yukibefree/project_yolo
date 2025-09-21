cc0fe36db213e933f267a1bb87d174aeee62420f
yuki.kunimoto <yuki.kunimoto@boxyz.com>
2025-09-21 14:55:57 +0900

    streamlitでリアルタイム解析

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,29 +2,31 @@ import streamlit as st
 import cv2
 import time
 import numpy as np
+import os
+import sys
+
+# プロジェクトルートの設定
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(root_dir)
+
+from tracker.tracker import Tracker
 
 def main():
     st.title("Webカメラ映像")
 
-    st.markdown("### 1. Webカメラ映像")
-    st.markdown("### 2. FPS表示付き映像")
-
     # 映像表示用のプレースホルダーを作成
-    col1, col2 = st.columns(2)
-    frame_placeholder1 = col1.empty()
-    frame_placeholder2 = col2.empty()
+    frame_placeholder1 = st.empty()
+    frame_placeholder2 = st.empty()
 
-    cap = cv2.VideoCapture(0) # 0は内蔵カメラ、複数ある場合は1,2と変える
+    tracker = Tracker()
 
-    if not cap.isOpened():
+    if not tracker.cap.isOpened():
         st.error("カメラが見つかりません。")
         return
 
-    st.sidebar.button("停止", on_click=lambda: cap.release() and st.stop())
-
     prev_time = time.time()
-    while cap.isOpened():
-        ret, frame = cap.read()
+    while tracker.cap.isOpened():
+        ret, frame = tracker.cap.read()
         if not ret:
             st.warning("フレームの取得に失敗しました。")
             break
@@ -33,21 +35,22 @@ def main():
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
         # 1つ目の映像 (そのまま表示)
-        frame_placeholder1.image(frame_rgb, channels="RGB", use_column_width=True)
+        frame_placeholder1.image(frame_rgb, channels="RGB", width='content')
 
         # 2つ目の映像 (FPS表示付き)
+        frame_processed = tracker.track(frame, server=False)
         current_time = time.time()
         fps = 1 / (current_time - prev_time)
         prev_time = current_time
         
         # FPSをフレームに描画
         fps_text = f"FPS: {fps:.2f}"
-        cv2.putText(frame, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(frame_processed, fps_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
         
-        frame_rgb_with_fps = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_placeholder2.image(frame_rgb_with_fps, channels="RGB", use_column_width=True)
+        frame_rgb_with_fps = cv2.cvtColor(frame_processed, cv2.COLOR_BGR2RGB)
+        frame_placeholder2.image(frame_rgb_with_fps, channels="RGB", width='content')
 
-    cap.release()
+    tracker.close_camera()
 
 if __name__ == "__main__":
     main()
